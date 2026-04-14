7f7a75d3fb33278e1ae133b813d408b8b7b774e0
ThureinS <94038134+ThureinS@users.noreply.github.com>
2026-04-14 13:39:31 +0700

    the whole app use context

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
 import { CartContext } from "./store/shopping-cart-context.jsx";
+import Product from "./components/Product.jsx";
 
 function App() {
   const [shoppingCart, setShoppingCart] = useState({
@@ -69,15 +70,19 @@ function App() {
   const ctxValue = {
     items: shoppingCart.items,
     addToCart: handleAddItemToCart,
+    onUpdateCartItemQuantity: handleUpdateCartItemQuantity,
   };
 
   return (
     <CartContext value={ctxValue}>
-      <Header
-        cart={shoppingCart}
-        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-      />
-      <Shop onAddItemToCart={handleAddItemToCart} />
+      <Header />
+      <Shop>
+        {DUMMY_PRODUCTS.map((product) => (
+          <li key={product.id}>
+            <Product {...product} />
+          </li>
+        ))}
+      </Shop>
     </CartContext>
   );
 }
